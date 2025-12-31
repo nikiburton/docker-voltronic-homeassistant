46be61f926786bfe19aa814162a97e2f869222f1
nikiburton <38562778+nikiburton@users.noreply.github.com>
2025-12-31 18:05:04 +0100

    Update mqtt-subscriber.sh

--- voltronic-addon/sources/inverter-mqtt/mqtt-subscriber.sh
+++ voltronic-addon/sources/inverter-mqtt/mqtt-subscriber.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-# --- RUTA CORREGIDA DEL JSON ---
-MQTT_JSON="/opt/inverter-mqtt/mqtt.json"
-
-MQTT_SERVER=`cat $MQTT_JSON | jq '.server' -r`
-MQTT_PORT=`cat $MQTT_JSON | jq '.port' -r`
-MQTT_TOPIC=`cat $MQTT_JSON | jq '.topic' -r`
-MQTT_DEVICENAME=`cat $MQTT_JSON | jq '.devicename' -r`
-MQTT_USERNAME=`cat $MQTT_JSON | jq '.username' -r`
-MQTT_PASSWORD=`cat $MQTT_JSON | jq '.password' -r`
-MQTT_CLIENTID=`cat $MQTT_JSON | jq '.clientid' -r`
+MQTT_SERVER=`cat /etc/inverter/mqtt.json | jq '.server' -r`
+MQTT_PORT=`cat /etc/inverter/mqtt.json | jq '.port' -r`
+MQTT_TOPIC=`cat /etc/inverter/mqtt.json | jq '.topic' -r`
+MQTT_DEVICENAME=`cat /etc/inverter/mqtt.json | jq '.devicename' -r`
+MQTT_USERNAME=`cat /etc/inverter/mqtt.json | jq '.username' -r`
+MQTT_PASSWORD=`cat /etc/inverter/mqtt.json | jq '.password' -r`
+MQTT_CLIENTID=`cat /etc/inverter/mqtt.json | jq '.clientid' -r`
 
 while read rawcmd;
 do
+
     echo "Incoming request send: [$rawcmd] to inverter."
-    # --- RUTA CORREGIDA DEL BINARIO ---
-    /usr/bin/inverter_poller -r $rawcmd;
+    /opt/inverter-cli/bin/inverter_poller -r $rawcmd;
 
 done < <(mosquitto_sub -h $MQTT_SERVER -p $MQTT_PORT -u "$MQTT_USERNAME" -P "$MQTT_PASSWORD" -i $MQTT_CLIENTID -t "$MQTT_TOPIC/sensor/$MQTT_DEVICENAME" -q 1)
